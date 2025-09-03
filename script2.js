08055c47ee934b19edad71b4485cf4355629f547
Z <z-webb@sbcglobal.net>
2025-09-02 22:26:50 -0500

    update sept 2

--- script2.js
+++ script2.js
@@ -1,25 +1,26 @@
-document.querySelectorAll('.project-desc').forEach((desc, index) => {
-    const fullText = desc.innerHTML;
-    const words = fullText.split(' ');
-  
-    if (words.length > 30) {
-      const shortText = words.slice(0, 30).join(' ') + '...';
-      desc.innerHTML = shortText;
-  
-      const btn = document.querySelectorAll('.see-more-btn')[index];
-  
-      btn.addEventListener('click', () => {
-        if (btn.innerText === 'See more') {
-          desc.innerHTML = fullText;
-          btn.innerText = 'See less';
-        } else {
-          desc.innerHTML = shortText;
-          btn.innerText = 'See more';
-        }
-      });
-    } else {
-      // Hide button if text is short
-      document.querySelectorAll('.see-more-btn')[index].style.display = 'none';
-    }
-  });
-  
+document.querySelectorAll('.card').forEach(card => {
+  const desc = card.querySelector('.project-desc');
+  const btn = card.querySelector('.see-more-btn');
+
+  if (!desc || !btn) return; // skip if either is missing
+
+  const fullText = desc.innerHTML;
+  const words = fullText.split(' ');
+
+  if (words.length > 30) {
+    const shortText = words.slice(0, 30).join(' ') + '...';
+    desc.innerHTML = shortText;
+
+    btn.addEventListener('click', () => {
+      if (btn.innerText === 'See more') {
+        desc.innerHTML = fullText;
+        btn.innerText = 'See less';
+      } else {
+        desc.innerHTML = shortText;
+        btn.innerText = 'See more';
+      }
+    });
+  } else {
+    btn.style.display = 'none';
+  }
+});
